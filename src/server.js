844550f6c4f28b8243fb2bf80bcfe50e2005a432
ankitguptacoding <ashokankitgupta9@gmail.com>
2023-07-06 01:09:20 +0530

    update redis

--- src/server.js
+++ src/server.js
@@ -5,6 +5,12 @@ const cors = require('cors');
 const router = require('../src/routes/userRoutes/userRoutes');
 const routerProduct = require('./routes/productRoutes/productRoutes');
 const app = express();
+const redis = require('redis');
+const redisClient = redis.createClient(6379,'127.0.0.1');
+redisClient.connect()
+redisClient.on("connect",(err)=>{
+console.log("redis connect");
+})
 
 app.use(express.json());
 app.use(cors());
