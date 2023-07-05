3ab410b1aea8c8dd2a33cb025cff82ae7e12f5bd
ankitguptacoding <ashokankitgupta9@gmail.com>
2023-07-06 01:19:43 +0530

    remove redis

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ const router = require('../src/routes/userRoutes/userRoutes');
 const routerProduct = require('./routes/productRoutes/productRoutes');
 const app = express();
 const redis = require('redis');
-const redisClient = redis.createClient(6379,'0.0.0.1');
-redisClient.connect()
-redisClient.on("connect",(err)=>{
-console.log("redis connect");
-})
+// const redisClient = redis.createClient(6379,'0  .0.0.1');
+// redisClient.connect()
+// redisClient.on("connect",(err)=>{
+// console.log("redis connect");
+// })
 
 app.use(express.json());
 app.use(cors());
