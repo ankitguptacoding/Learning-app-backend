913599434e60ff9c9440f440f222e31626e5dd4b
ankitguptacoding <ashokankitgupta9@gmail.com>
2023-07-05 23:23:44 +0530

    Initial commit

---
+++ src/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    type: String,
+    mobile: String
+
+});
+
+module.exports = mongoose.model('users',userSchema);
---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require("dotenv").config();
+require('././config/database');
+const cors = require('cors');
+const router = require('../src/routes/userRoutes/userRoutes');
+const routerProduct = require('./routes/productRoutes/productRoutes');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+console.log("server start")
+//default url 
+app.use('/', router,routerProduct);
+
+app.listen(450);
