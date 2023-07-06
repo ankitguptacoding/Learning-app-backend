448263d02f8df03076d2489c131bbbb353423099
ankitguptacoding <ashokankitgupta9@gmail.com>
2023-07-06 16:55:56 +0530

    remove s3

--- src/admin/routes/bannerRoutes.js
+++ src/admin/routes/bannerRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/authMiddleware');
-const { uploadFileToS3 }  = require('../../middleware/aws');
+// const { uploadFileToS3 }  = require('../../middleware/aws');
 const { uploadBanner, uploadLatestVideos, uploadStudyMaterailPdf } = require('../controller/bannerController');
 
 // Feature Banner  Route
-router.route("/api/uploadBanner").post(auth, uploadFileToS3.single('banner_image'), uploadBanner);
+// router.route("/api/uploadBanner").post(auth, uploadFileToS3.single('banner_image'), uploadBanner);
 
 //  Latest Videos Route
 router.route("/api/uploadLatestVideos").post(auth,uploadLatestVideos);
