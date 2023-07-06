06c2ad7ea6483c314f4df9465eab0a8269408319
ankitguptacoding <ashokankitgupta9@gmail.com>
2023-07-06 12:55:00 +0530

    update redis

---
+++ src/redis.js
@@ -0,0 +1,14 @@
+const redisClient = require('redis');
+var redis = redisClient.createClient({
+    url: process.env.EXTERNAL_REDIS_URL
+});
+redis.on("connect", function () {
+    console.log("Redis Server connected");
+});
+ redis.connect();
+redis.set('key', '12345');
+const fun = async ()=>{
+    const value = await redis.get('key');
+    console.log("RedisValue",value);
+}
+module.exports = fun()
--- src/server.js
+++ src/server.js
@@ -5,16 +5,13 @@ const cors = require('cors');
 const router = require('../src/routes/userRoutes/userRoutes');
 const routerProduct = require('./routes/productRoutes/productRoutes');
 const app = express();
-const redis = require('redis');
-// const redisClient = redis.createClient(6379,'0  .0.0.1');
-// redisClient.connect()
-// redisClient.on("connect",(err)=>{
-// console.log("redis connect");
-// })
-
+const redisFun = require('../src/redis')
 app.use(express.json());
 app.use(cors());
 console.log("server start")
+const redisTest = async()=>{
+    await redisFun()
+}
 //default url 
 app.use('/', router,routerProduct);
 
