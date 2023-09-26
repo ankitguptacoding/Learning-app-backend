d8589b7dc3392e8657ddb172c0f161e3e9e117de
ankitguptacoding <ashokankitgupta9@gmail.com>
2023-09-26 23:16:28 +0530

    change aws key

--- src/middleware/aws.js
+++ src/middleware/aws.js
@@ -4,11 +4,11 @@ const multer = require('multer')
 const multerS3 = require('multer-s3')
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_NEW,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_NEW,
   region: process.env.AWS_REGION
 })
-console.log(process.env.AWS_ACCESS_KEY_ID ," ",process.env.AWS_SECRET_ACCESS_KEY," ",process.env.AWS_REGION," ", process.env.AWS_BUCKET_NAME)
+console.log(process.env.AWS_ACCESS_KEY_ID_NEW ," ",process.env.AWS_SECRET_ACCESS_KEY_NEW," ",process.env.AWS_REGION," ", process.env.AWS_BUCKET_NAME_NEW)
 const multerS3config = multerS3({
   s3,
   bucket: process.env.AWS_BUCKET_NAME,
